a82f76b08e8e1d2fd7f069bcfa5f64a66a7876a2
Yulia Kruhlyk <100228348+yulia-kr@users.noreply.github.com>
2024-05-02 16:13:00 +0200

    Add files via upload

---
+++ ice_breaker.py
@@ -0,0 +1,46 @@
+from typing import Tuple
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from chains.custom_chains import (
+    get_summary_chain,
+    get_interests_chain,
+    get_ice_breaker_chain,
+)
+from third_parties.linkedin import scrape_linkedin_profile
+from output_parsers import (
+    Summary,
+    IceBreaker,
+    TopicOfInterest,
+)
+
+
+def ice_break_with(
+    name: str,
+) -> Tuple[Summary, TopicOfInterest, IceBreaker, str]:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+
+    summary_chain = get_summary_chain()
+    summary_and_facts: Summary = summary_chain.invoke(
+        input={"information": linkedin_data},
+    )
+
+    interests_chain = get_interests_chain()
+    interests: TopicOfInterest = interests_chain.invoke(
+        input={"information": linkedin_data}
+    )
+
+    ice_breaker_chain = get_ice_breaker_chain()
+    ice_breakers: IceBreaker = ice_breaker_chain.invoke(
+        input={"information": linkedin_data}
+    )
+
+    return (
+        summary_and_facts,
+        interests,
+        ice_breakers,
+        linkedin_data.get("profile_pic_url"),
+    )
+
+
+if __name__ == "__main__":
+    pass
